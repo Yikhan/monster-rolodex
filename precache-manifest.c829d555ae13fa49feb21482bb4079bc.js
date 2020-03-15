6d1393c71f64d5f9ad5eb1f03323e9e0bf16fdce
Yikhan <yhxiaoch@gmail.com>
2020-03-15 22:43:30 +1100

    Updates

---
+++ precache-manifest.c829d555ae13fa49feb21482bb4079bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90c9d52c63eda4a661b1d2d4957488d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f316c4410d229edc6428",
+    "url": "/monster-rolodex/static/css/main.64702ebe.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/monster-rolodex/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f316c4410d229edc6428",
+    "url": "/monster-rolodex/static/js/main.8f501f57.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
